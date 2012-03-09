34383a2b90c92b84a8e9657ffba2b3e99683db6b
Neal Riley <nriley@academyart.edu>
2012-03-09 10:40:38 -0800

    Updated SafariTest

--- safari_test.rb
+++ safari_test.rb
@@ -1,13 +1,33 @@
 require 'rubygems'
 require 'safariwatir'
 
-server_url = "http://discussion.academyart.edu/"
-username = "nriley"
+puts "Called with: " + ARGV[0] + " , " + ARGV[1] 
+username = ARGV[0]
+password = ARGV[1] 
+if ARGV[2] == "post"
+  puts "calling on QA-Post"
+  server = "http://qa-post-production.dev.academyart.edu"
+elsif ARGV[2] == "pre"
+  puts "calling on QA-Pre"
+  server = "http://qa-pre-production.dev.academyart.edu"
+else
+  puts "Use a third ARg!"
+end
 
-safari = Watir::Safari.new
-safari.goto server_url
+######################################################
+#
+#
+#
+#
+#
+#
+#
+######################################################
 
-#Log In
-safari.text_field(:name, 'username').set username
-safari.text_field(:name, 'password').set ''
-safari.button(:name => 'buttonWrapper:login').click
+browser = Watir::Safari.new
+browser.goto(server)
+browser.text_field(:name, "username").set(username)
+browser.password(:name, "password").set(password)
+browser.button(:name, "buttonWrapper:login").click
+
+puts "FAILURE" unless browser.contains_text("Current")
